2e66ebfb3235ca38afaaa58ca812a0e961b7a156
kejlor <woj450@gmail.com>
2023-02-27 12:31:01 +0100

    Initial Commit

---
+++ InstagramUI/InstagramUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUIApp.swift
+//  InstagramUI
+//
+//  Created by Bartosz Wojtkowiak on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
